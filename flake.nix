802dc891a91a1eb3d31e090041d07f962cb84196
Steffen Trumtrar <s.trumtrar@pengutronix.de>
2024-06-14 10:14:51 +0200

    github: update workflow
    Signed-off-by: Steffen Trumtrar <s.trumtrar@pengutronix.de>

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs =
     {
+      nixpkgs.url = "gitub:nixos/nixpkgs/master";
       flake-utils.url = "github:numtide/flake-utils";
       nix-environments.url = "github:nix-community/nix-environments";
       davepkgs.url = "github:danderson/nixpkgs/openfpgaloader";
